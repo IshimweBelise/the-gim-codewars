b350f444774128ccfad9d72a4d45b094d747b3f1
ishimwe belise <ishimwebe20@gmail.com>
2025-06-01 06:49:31 -0700

    Added changes

---
+++ 8kyu/nadine/the-gym/TheGym.js
@@ -0,0 +1,11 @@
+function checkEvenOrOdd(num){
+ let num=o;
+    if (num % 2===0){
+        console.log('the number is an even number');
+    }else {
+        console.log('the number is an odd number');
+    };
+    };
+
+checkEvenOrOdd()
+
